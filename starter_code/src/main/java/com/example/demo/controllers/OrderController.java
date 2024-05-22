adde2947eb5d160b83dca3345b83d316ef731b0a
Sarah176 <120200645+Sarah176@users.noreply.github.com>
2024-05-22 13:00:22 +0300

    splunk integration

--- starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,32 +22,46 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
-	
+	Logger log = LogManager.getLogger(OrderController.class);
+
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	
+
+
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
-		User user = userRepository.findByUsername(username);
-		if(user == null) {
-			return ResponseEntity.notFound().build();
+		try {
+			User user = userRepository.findByUsername(username);
+			if (user == null) {
+				log.error("SubmitOrderFail Error in submitting order User {} ", username + " was not found");
+				return ResponseEntity.notFound().build();
+			}
+			UserOrder order = UserOrder.createFromCart(user.getCart());
+			orderRepository.save(order);
+			log.info("SubmitOrderSuccess {} ", order.toString() + " was submitted successfully");
+			return ResponseEntity.ok(order);
+		} catch (Exception e) {
+			log.error("SubmitOrderFail Error in submitting order {}", e.getMessage());
+			return ResponseEntity.badRequest().build();
 		}
-		UserOrder order = UserOrder.createFromCart(user.getCart());
-		orderRepository.save(order);
-		return ResponseEntity.ok(order);
 	}
-	
+
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
-		User user = userRepository.findByUsername(username);
-		if(user == null) {
-			return ResponseEntity.notFound().build();
+		try {
+			User user = userRepository.findByUsername(username);
+			if (user == null) {
+				log.error("GetOrderHistoryFail User {} ", username + " was not found");
+				return ResponseEntity.notFound().build();
+			}
+			return ResponseEntity.ok(orderRepository.findByUser(user));
+		} catch (Exception e) {
+			log.error("GetOrderHistoryFail  {}", e.getMessage());
+			return ResponseEntity.badRequest().build();
 		}
-		return ResponseEntity.ok(orderRepository.findByUser(user));
 	}
 }
--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -20,13 +20,14 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-@Slf4j
 public class UserController {
-	
+	Logger log= LogManager.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -36,30 +37,42 @@ public class UserController {
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+
 		return ResponseEntity.of(userRepository.findById(id));
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+		if(user == null) {
+			log.error("UserController:findByUserName User {} ",username+" was not found");
+			return ResponseEntity.notFound().build();
+		}
+		return  ResponseEntity.ok(user);
 	}
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
-		User user = new User();
-		user.setUsername(createUserRequest.getUsername());
-		Cart cart = new Cart();
-		cartRepository.save(cart);
-		user.setCart(cart);
-		if(createUserRequest.getPassword().length()<7 ||
-				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-			log.error("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create {}",createUserRequest.getUsername());
+		try {
+			User user = new User();
+			user.setUsername(createUserRequest.getUsername());
+			Cart cart = new Cart();
+			cartRepository.save(cart);
+			user.setCart(cart);
+			if (createUserRequest.getPassword().length() < 7 ||
+					!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+				log.error("CreateUserFail Error - Either length is less than 7 or pass and conf pass do not match. Unable to create {}", createUserRequest.getUsername());
+				return ResponseEntity.badRequest().build();
+			}
+			user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+			userRepository.save(user);
+
+			log.info("CreateUserSuccess User {} ", user.getUsername()+" was created successfully");
+			return ResponseEntity.ok(user);
+		}catch (Exception e){
+			log.error("CreateUserFail Error in creating user {}",e.getMessage());
 			return ResponseEntity.badRequest().build();
 		}
-		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
-		userRepository.save(user);
-		return ResponseEntity.ok(user);
 	}
 	
 }
